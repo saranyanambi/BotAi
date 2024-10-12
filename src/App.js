d423a4a809b9e68e76878e4860bf611a22c7667d
saranya <saranyanambi911@gmail.com>
2024-10-12 16:56:08 +0530

    BotBody added and some conditions implemented

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Components/SideBar/Sidebar';
 import ChatBody from './Components/ChatBody/ChatBody';
+import { useState } from 'react';
+import BotBody from './Components/BotBody/BotBody';
 
 function App() {
+  const [sidebarOn,setSidebaron]=useState(false);
+  const [currentChat,setCurrentChat]=useState([]);
+  const [pastConvo,setPastConvo]=useState(false);
+
+  const clearChat=()=>setCurrentChat([]);
+  const handlePastConvo=()=>setPastConvo(!pastConvo);
+  const handleSideBar=()=>setSidebaron(!sidebarOn);
+  const chatWithBot=(userMsg,botMsg)=>{
+      setCurrentChat([...currentChat,userMsg,botMsg]);
+  }
   return (
     <div className="App">
         <Sidebar/>
-        <ChatBody/>
+        <BotBody sidebarOn={sidebarOn} clearChat={clearChat} pastConvo={pastConvo} handleSideBar={handleSideBar} currentChat={currentChat} chatWithBot={chatWithBot}/>
     </div>
   );
 }
