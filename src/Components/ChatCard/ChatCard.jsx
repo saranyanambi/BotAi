0bb8790343819c0155d69d9785ff5c560fc4f2e0
saranya <saranyanambi911@gmail.com>
2024-10-13 11:50:31 +0530

    Update comment passes as prop and rating & feedback form add

--- src/Components/ChatCard/ChatCard.jsx
+++ src/Components/ChatCard/ChatCard.jsx
@@ -1,8 +1,55 @@
+import { useState } from "react";
 import "./ChatCard.css";
+import FeedbackForm from "../FeedbackForm/FeedbackForm";
+import Modal from "react-modal";
+import Rating from "@mui/material/Rating";
 
 const ChatCard=(props)=>{
-    const {icon,name,msg,like,dislike,id,customClass,time,feedback}=props;
+    const {icon,name,msg,like,dislike,id,customClass,time,feedback,currentChat,updateComments}=props;
+
+    console.log(id);
+    const [showFeedback,setShowfeedback]=useState(false);
+    const [userFeedback,setUsefeedback]=useState("")
     console.log(customClass);
+    const [isModelOpen,setIsModelOpen]=useState(false);
+    const [showRating,setShowRating]=useState(false)
+    const [rating,setRating]=useState(0);
+
+    const modalStyle={
+        content:{
+            top:"30%",
+            left:"40%",
+            right:"auto",
+            bottom:"auto",
+            transform:"translate("-50%","-50%")",
+            width:"80%",
+            maxWidth:"500px",
+            background:"rgba(255,255,255,0.6)",
+            borderRadius:"1rem",
+            
+        }
+    }
+
+    const handle=(e,feedback)=>{
+        e.preventDefault()
+        setUsefeedback(feedback)
+        setIsModelOpen(false)
+        setShowfeedback(false)
+        const newComment=userFeedback;
+        updateComments(id,newComment)
+
+        console.log(feedback,"submit");
+    }
+
+    const handleFeedbackOpen=()=>{
+        setShowfeedback(true)
+        setIsModelOpen(true)
+    }
+    const handleRating=()=>{
+        setShowRating(true)
+        setRating(prev=>Math.min(prev+1,5))
+        
+    }
     return(
         <div className={`chatcard-container ${customClass}`}>
             <span>
@@ -10,19 +57,61 @@ const ChatCard=(props)=>{
             </span>
             <span className="chatcard-text">
             <span className="chatcard-name">{name}</span>
-            <span className="message">{msg}</span>
-            </span>
-            <span>{time}</span>
-            <span>{name=="Soul AI"?<>
-            <img src={feedback} alt="feedback" className="feedback"/>
-            <img src={like} alt="img"/>
+            <div className="msg-container">
+            <span className="message">{msg}
+            <div className="interaction_icon">
+            {name=="Soul AI" && (
+                <>
+            <img src={feedback} alt="feedback" className="feedback" onClick={()=>handleFeedbackOpen()}/>
+            <img src={like} alt="img" onClick={()=>handleRating()}/>
             <img src={dislike} alt="dislike"/>
-            </>
-            :
-            ""}
+            
+            </>)}
+            </div>
             </span>
+            </div>
+            </span>
+            
+            
+           
+           
+            <span>{time}</span>
+        
+            <div>{userFeedback.length>0?userFeedback:""}</div>
+            {showRating &&
+            <Rating
+            value={rating}
+            onChange={(e,newValue)=>{
+                setRating(newValue)
+            }}
+            precision={1}/>
+        }
+            <Modal
+            isOpen={isModelOpen}
+            onRequestClose={()=>setIsModelOpen(false)}
+            style={modalStyle}
+            contentLabel="Feedback">
+           { showFeedback && 
+                     <form onSubmit={(e)=>handle(e,userFeedback)}>
+                     <textarea
+                     value={userFeedback}
+                     placeholder="Share your feedback"
+                     onChange={e=>setUsefeedback(e.target.value)}
+                     required
+                     className="textarea"
+                     />
+                    <div className="btn-container">
+                     <button type="submit" className="submit">Submit</button>
+                
+                     <button onClick={()=>setIsModelOpen(false)} className="cancel">Cancel</button>
+
+                     </div>
+                 </form>
+                 }
+        </Modal>
         </div>
     )
 }
 
 export default ChatCard;
+
