72ee07e356009c8969375d079fad721c93578abf
saranya <saranyanambi911@gmail.com>
2024-10-13 11:49:33 +0530

    Update comment passes as prop

--- src/Components/BotBody/BotBody.jsx
+++ src/Components/BotBody/BotBody.jsx
@@ -3,12 +3,12 @@ import ChatBody from "../ChatBody/ChatBody";
 import PastConvo from "../PastConvo/PastConvo";
 import "./BotBody.css";
 const BotBody=(props)=>{
-    const {sidebarOn,clearChat,pastConvo,handleSideBar,currentChat,chatWithBot}=props;
+    const {sidebarOn,clearChat,pastConvo,handleSideBar,currentChat,chatWithBot,updateComments}=props;
     return(
         <div className="BotBody-container">
            
             <div>
-               { pastConvo?<PastConvo/>:<ChatBody currentChat={currentChat} chatWithBot={chatWithBot} clearChat={clearChat}/>}
+               { pastConvo?<PastConvo/>:<ChatBody currentChat={currentChat} chatWithBot={chatWithBot} clearChat={clearChat} updateComments={updateComments}/>}
             </div>
         </div>
     )
