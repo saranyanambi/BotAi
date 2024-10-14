d4fab805289d3c88a0e7851829287b8d754245f1
saranya <saranyanambi911@gmail.com>
2024-10-14 11:02:40 +0530

    pastconvo created to show saved conversations

--- src/Components/PastConvo/PastConvo.jsx
+++ src/Components/PastConvo/PastConvo.jsx
@@ -1,6 +1,70 @@
-const PastConvo=()=>{
-    return(
-        <h1>hi</h1>
-    )
-}
+
+import { useState, useEffect } from "react";
+import ConversationCard from "../CoversationCard/ConversationCard";
+import { fetchStar } from "../Commonfunction/function";
+import "./PastConvo.css";
+
+const PastConvo = () => {
+    const [convos, setConvos] = useState([]);
+    const [starSelect, setStarSelect] = useState("");
+    const [filteredConvo, setFilteredConvo] = useState([]);
+
+    useEffect(() => {
+        const fetchLocal = () => {
+            const localCovo = localStorage.getItem("pastconversation");
+            if (localCovo) {
+                const parsedConvos = JSON.parse(localCovo);
+                setConvos(parsedConvos);
+                setFilteredConvo(parsedConvos); 
+            }
+        };
+        fetchLocal();
+    }, []);
+
+    const handleStarSelect = (e) => {
+        const selectedStar = e.target.value;
+        setStarSelect(selectedStar);
+
+        if (selectedStar) {
+            const res = fetchStar(selectedStar, convos);
+            setFilteredConvo(res);
+            console.log(res)
+        } else {
+            setFilteredConvo(convos); 
+        }
+    };
+
+    const displayCards = (filteredConvo) => filteredConvo.map(item => {
+        const { id, conversation } = item;
+
+        return <ConversationCard key={id} conversation={conversation} />;
+    });
+
+    return (
+        <div className="pastconvo-wrapper">
+            <h3>Conversation History</h3>
+            <div className="select">
+                <select 
+                    name="star" 
+                    id="start" 
+                    value={starSelect} 
+                    onChange={handleStarSelect}
+                >
+                    <option value="">Select Star</option>
+                    <option value="1">1 Star</option>
+                    <option value="2">2 Star</option>
+                    <option value="3">3 Star</option>
+                    <option value="4">4 Star</option>
+                    <option value="5">5 Star</option>
+                </select>
+            </div>
+            <p>Today's Chat</p>
+            <div className="pastconvo-container">
+                {filteredConvo.length ? displayCards(filteredConvo) : "No conversations found."}
+            </div>
+        </div>
+    );
+};
+
 export default PastConvo;
+
