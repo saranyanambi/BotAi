777ef28330caf07283e6379f46f4c743a19e3959
saranya <saranyanambi911@gmail.com>
2024-10-14 11:01:11 +0530

    fetchrating function implemented

--- src/Components/Commonfunction/function.js
+++ src/Components/Commonfunction/function.js
@@ -13,3 +13,43 @@ export const time=()=>{
     hour=hour?hour:12;
     return hour.toString().padStart(2,'0')+":"+min.toString().padStart(2,'0')+" "+ampm;
 }
+
+export const saveLocal=(currentChat)=>{
+    const newConvo={id:`save-${Date.now()}`,conversation:currentChat}
+    let saveConvo=[newConvo]
+
+    const pastChat=JSON.parse(localStorage.getItem("pastconversation"));
+    if(pastChat)
+    {
+        saveConvo=[newConvo,...pastChat];
+
+    }
+    localStorage.setItem("pastconversation",JSON.stringify(saveConvo));
+}
+
+export const fetchStar=(star,convo)=>{
+        return convo.map(item=>{
+            const filterconversation= item.conversation.filter(ele=>ele.rating==star)
+
+            return{
+                id:item.id,
+                conversation:filterconversation
+            }
+        
+        })
+}
+
+// export const fetchStar = (rating,conversations) => {
+//     console.log(conversations)
+//     return conversations
+//     .filter(conv => 
+//         conv.conversation.some(msg => msg.rating === rating) // Filter conversations
+//     )
+//     .map(conv => ({
+//         ...conv,
+//         conversation: conv.conversation.filter(msg => msg.rating === rating) // Keep only messages with selected rating
+//     }));
+        
+// };
+
+
