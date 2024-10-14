982f04cdd99662cc007bdab3d98cf3d2aec2c489
saranya <saranyanambi911@gmail.com>
2024-10-14 17:31:43 +0530

    hadleform passed as props

--- src/Components/IntroCard/IntroCard.jsx
+++ src/Components/IntroCard/IntroCard.jsx
@@ -1,8 +1,8 @@
 import "./IntroCard.css";
 const IntroCard=(props)=>{
-    const {text,subtext}=props;
+    const {text,subtext,handleForm}=props;
     return(
-        <div className="into-card-container">
+        <div className="into-card-container" onClick={()=>handleForm(text)}>
             <span className="text">{text}</span>
             <span className="subtext">{subtext}</span>
         </div>
