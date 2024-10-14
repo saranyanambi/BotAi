0386e0b8e343f39ec617572f07ee08cba48d63b9
saranya <saranyanambi911@gmail.com>
2024-10-14 11:00:16 +0530

    conversation card created to show the pastconvo card

---
+++ src/Components/CoversationCard/ConversationCard.jsx
@@ -0,0 +1,20 @@
+import ChatCard from "../ChatCard/ChatCard";
+import "./ConversationCard.css"
+const ConversationCard=(props)=>{
+        const {conversation}=props;
+
+        const displayconvo=()=>{
+                return conversation.map(item=>{
+                    const {icon,name,msg,like,dislike,id,time,feedback,comments,rating}=item;
+                    return <ChatCard convo="true" icon={icon} name={name} msg={msg} like={like} dislike={dislike} id={id}  time={time} feedback={feedback} comment={comments} rating={rating}/>
+                })
+        }
+
+        return(
+            <div className="convoCard">
+                {displayconvo()}
+            </div>
+        )
+}
+
+export default ConversationCard;
