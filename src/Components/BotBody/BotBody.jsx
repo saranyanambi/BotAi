47a85b01e7ab61ef958bbdbebb7cc29e29206e79
saranya <saranyanambi911@gmail.com>
2024-10-14 10:55:22 +0530

    updaterating passed as prop

--- src/Components/BotBody/BotBody.jsx
+++ src/Components/BotBody/BotBody.jsx
@@ -3,12 +3,12 @@ import ChatBody from "../ChatBody/ChatBody";
 import PastConvo from "../PastConvo/PastConvo";
 import "./BotBody.css";
 const BotBody=(props)=>{
-    const {sidebarOn,clearChat,pastConvo,handleSideBar,currentChat,chatWithBot,updateComments}=props;
+    const {sidebarOn,clearChat,pastConvo,handleSideBar,currentChat,chatWithBot,updateComments,updateRating}=props;
     return(
         <div className="BotBody-container">
            
             <div>
-               { pastConvo?<PastConvo/>:<ChatBody currentChat={currentChat} chatWithBot={chatWithBot} clearChat={clearChat} updateComments={updateComments}/>}
+               { pastConvo?<PastConvo/>:<ChatBody currentChat={currentChat} chatWithBot={chatWithBot} clearChat={clearChat} updateComments={updateComments} updateRating={updateRating}/>}
             </div>
         </div>
     )
