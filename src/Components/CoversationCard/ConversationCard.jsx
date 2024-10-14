ffff14d79835a8ee2a0154af561e849301b45c27
saranya <saranyanambi911@gmail.com>
2024-10-14 17:29:46 +0530

    change the style

--- src/Components/CoversationCard/ConversationCard.jsx
+++ src/Components/CoversationCard/ConversationCard.jsx
@@ -5,6 +5,7 @@ const ConversationCard=(props)=>{
 
         const displayconvo=()=>{
                 return conversation.map(item=>{
+                    console.log(item)
                     const {icon,name,msg,like,dislike,id,time,feedback,comments,rating}=item;
                     return <ChatCard convo="true" icon={icon} name={name} msg={msg} like={like} dislike={dislike} id={id}  time={time} feedback={feedback} comment={comments} rating={rating}/>
                 })
