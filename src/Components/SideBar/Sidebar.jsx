940521d5770f13f6c1f662f761421a601740b4f5
saranya <saranyanambi911@gmail.com>
2024-10-14 11:08:18 +0530

    props are passed in sidebar

--- src/Components/SideBar/Sidebar.jsx
+++ src/Components/SideBar/Sidebar.jsx
@@ -1,11 +1,13 @@
 import bot_icon from "../../assets/bot_icon.png";
 import edit_icon from "../../assets/edit_icon.png";
 import "./Sidebar.css";
-const Sidebar=()=>{
+const Sidebar=(props)=>{
+
+    const {handlePastConvo,sidebarOn,handleSideBar,newChat}=props
 
     return(
     <div className="sidebar-wrapper">
-    <div className="sidebar-container">
+    <div className="sidebar-container" onClick={newChat}>
         <div>
             <img src={bot_icon} alt="bot_icon" className="bot_icon"/>
         </div>
@@ -17,7 +19,7 @@ const Sidebar=()=>{
         </div>
     </div>
     <div>
-        <button className="past-convo"> Past Conversation </button>
+        <button className="past-convo" onClick={handlePastConvo}> Past Conversation </button>
     </div>
     </div>
     )
