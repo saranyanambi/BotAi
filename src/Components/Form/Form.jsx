38ee3d750dd0c69b6c69d042e1c0fd2a712dd52a
saranya <saranyanambi911@gmail.com>
2024-10-14 11:02:01 +0530

    handling the save button

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Form.css";
 const Form=(props)=>{
-    const {handleForm}=props;
+    const {handleForm,handleSave}=props;
     const [query,setQuery]=useState("");
     const handleSubmit=(e)=>{
         e.preventDefault()
@@ -14,7 +14,7 @@ const Form=(props)=>{
         <input name="question" onChange={e=>setQuery(e.target.value)}/>
         <button type="submit" className="btn">Ask</button>
     </form>
-    <button type="submit" className="btn">Save</button>
+    <button type="submit" className="btn" onClick={handleSave}>Save</button>
     </div>
     )
 }
